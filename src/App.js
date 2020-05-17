f9c773b609b4034741a59d185b12714d6d060247
Chia Huang <chialh@uci.edu>
2020-05-16 23:12:55 -0700

    first commit - initial commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Happy Coding!!! Testing 1 2 3 !!!
         </p>
         <a
           className="App-link"
